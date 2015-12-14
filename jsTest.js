b50eff17c111c315f20342375fdbb50140ed22c5
Spencer Lewis <Spencerlewis987@gmail.com>
2015-12-14 09:39:19 -0500

    finished MVP basic functionality, to add AI

---
+++ jsTest.js
@@ -0,0 +1,17 @@
+//Win Conditions
+(board[0][0], board[0][1], board[0][2])
+(board[1][0], board[1][1], board[1][2])
+(board[2][0], board[2][1], board[2][2])
+(board[0][0], board[1][1], board[2][2])
+(board[2][0], board[1][1], board[0][2])
+(board[0][2], board[1][2], board[2][2])
+(board[0][1], board[1][1], board[2][1])
+(board[0][0], board[1][0], board[2][0])
+
+//illegal move stuff
+  if (($(this).attr('class')) != 'box') {
+    alert('This square has been played already! play a different square!');
+
+
+//print turn stuff
+if (i % 2 === 0)
---
+++ script/script.js
@@ -0,0 +1,157 @@
+var board = [[null, null, null],
+             [null, null, null],
+             [null, null, null]];
+
+$(function(){
+  var i = 0;
+
+
+  function getWinner() {
+  //cond1 x
+  if (board[0][0] === 'x' && board[0][1] === 'x' && board[0][2] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+  //cond2 x
+  if (board[1][0] === 'x' && board[1][1] === 'x' && board[1][2] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+  //cond3 x
+  if (board[2][0] === 'x' && board[2][1] === 'x' && board[2][2] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+  //cond4 x
+  if (board[0][0] === 'x' && board[1][1] === 'x' && board[2][2] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+  //cond5 x
+  if (board[2][0] === 'x' && board[1][1] === 'x' && board[0][2] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+  //cond6 x
+  if (board[0][2] === 'x' && board[1][2] === 'x' && board[2][2] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+  //cond7 x
+  if (board[0][1] === 'x' && board[1][1] === 'x' && board[2][1] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+
+//cond8 x
+  if (board[0][0] === 'x' && board[1][0] === 'x' && board[2][0] === 'x') {
+    alert('X Wins! O Loses!');
+  }
+  //cond1 o
+  if (board[0][0] === 'o' && board[0][1] === 'o' && board[0][2] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+  //cond2 o
+  if (board[1][0] === 'o' && board[1][1] === 'o' && board[1][2] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+  //cond3 o
+  if (board[2][0] === 'o' && board[2][1] === 'o' && board[2][2] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+  //cond4 o
+  if (board[0][0] === 'o' && board[1][1] === 'o' && board[2][2] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+  //cond5 o
+  if (board[2][0] === 'o' && board[1][1] === 'o' && board[0][2] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+  //cond6 o
+  if (board[0][2] === 'o' && board[1][2] === 'o' && board[2][2] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+  //cond7 o
+  if (board[0][1] === 'o' && board[1][1] === 'o' && board[2][1] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+
+//cond8 o
+  if (board[0][0] === 'o' && board[1][0] === 'o' && board[2][0] === 'o') {
+    alert('O Wins! X Loses!');
+  }
+}
+
+  $('.box').on('click', function() {
+    // while (i < 99999) {
+    //THIS MIGHT BE THE SPOT TO RUN THE ILLEGAL MOVE FUNCTION
+    if (($(this).attr('class')) != 'box') {
+    alert('This square has been played already! Play a different square!');
+  }
+    else {
+      if (i % 2 === 0) {
+        $('h2').html('Turn : O');
+        $(this).toggleClass('x');
+        if (div1.className === 'box x') {
+            board[0][0] = 'x';
+        }
+        if (div2.className === 'box x') {
+            board[0][1] = 'x';
+        }
+        if (div3.className === 'box x') {
+            board[0][2] = 'x';
+        }
+        if (div4.className === 'box x') {
+            board[1][0] = 'x';
+        }
+        if (div5.className === 'box x') {
+            board[1][1] = 'x';
+        }
+        if (div6.className === 'box x') {
+            board[1][2] = 'x';
+        }
+        if (div7.className === 'box x') {
+            board[2][0] = 'x';
+        }
+        if (div8.className === 'box x') {
+            board[2][1] = 'x';
+        }
+        if (div9.className === 'box x') {
+            board[2][2] = 'x';
+        }
+        getWinner();
+        return i++;
+      }
+      else {
+        $(this).toggleClass('o');
+        $('h2').html('Turn: X');
+        if (div1.className === 'box o') {
+            board[0][0] = 'o';
+        }
+        if (div2.className === 'box o') {
+            board[0][1] = 'o';
+        }
+        if (div3.className === 'box o') {
+            board[0][2] = 'o';
+        }
+        if (div4.className === 'box o') {
+            board[1][0] = 'o';
+        }
+        if (div5.className === 'box o') {
+            board[1][1] = 'o';
+        }
+        if (div6.className === 'box o') {
+            board[1][2] = 'o';
+        }
+        if (div7.className === 'box o') {
+            board[2][0] = 'o';
+        }
+        if (div8.className === 'box o') {
+            board[2][1] = 'o';
+        }
+        if (div9.className === 'box o') {
+            board[2][2] = 'o';
+        }
+        getWinner();
+        return i++;
+      }
+    }
+  });
+
+  $('#refresh').on('click', function() {
+      location.reload(true);
+  });
+});
